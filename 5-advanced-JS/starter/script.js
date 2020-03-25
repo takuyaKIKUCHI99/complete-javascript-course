dda770d2b8ab7a4b3b800554ba2f848aaf35e72e
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-25 12:10:05 +0900

    challenge1 refactored

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,12 +1,13 @@
+// 7 IIFE
 (function() {
-  // 1
+  // 1 Build a function constructor
   function Question(question, answers, correctAnswer) {
     this.question = question;
     this.answers = answers;
     this.correctAnswer = correctAnswer;
   }
 
-  // 2
+  // 2 Create questions
   const question1 = new Question(
     "What is my name?",
     ["Takuya", "Mac", "Tom"],
@@ -18,26 +19,31 @@
     ["futsal", "soccer", "wine", "movie", "All"],
     4
   );
-  // 3
+
+  // 3 Store questions in array
   const questions = [question1, question2, question3];
 
-  // 4
+  // 4 Display a randomly picked question and the answers
   const randomPick = questions[Math.floor(Math.random() * questions.length)];
-  console.log(randomPick.question);
-  randomPick.answers.forEach((answer, i) => {
-    console.log(`${i + 1}: ${answer}`);
-  });
 
-  // 5
+  Question.prototype.displayQuestions = function() {
+    console.log(this.question);
+    this.answers.forEach((answer, i) => {
+      console.log(`${i + 1}: ${answer}`);
+    });
+  };
+  randomPick.displayQuestions();
+
+  // 5 Get user response with prompt
   const response = window.prompt("Please input number for the correct answer");
-  checkCorrect(response, randomPick);
 
-  // 6
-  function checkCorrect(response, randomPick) {
-    if (response - 1 === randomPick.correctAnswer) {
+  // 6 Check if correct or not
+  Question.prototype.checkCorrect = function(response) {
+    if (response - 1 === this.correctAnswer) {
       console.log("Correct!!");
     } else {
       console.log("Wrong. Try again");
     }
-  }
+  };
+  randomPick.checkCorrect(response);
 })();
